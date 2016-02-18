3815dc74fa64506289fe9f0a39c47d6d5e0ccb77
AnnaSpring <annavv.0595@gmail.com>
2016-02-18 02:04:41 +0200

    Test commit

---
+++ 1 Module Java Basics/src/distanceBetween/Distance.java
@@ -0,0 +1,35 @@
+package distanceBetween;
+
+import java.io.*;
+import java.util.*;
+
+public class Distance {
+	public static void main(String[] args) throws Exception{
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		inputDigits(list);
+		for(int i = 0; i< list.size(); i++){
+			System.out.println(list.get(i));
+		}
+		
+	}
+	
+	public static ArrayList<Integer> inputDigits(ArrayList<Integer> list) throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String string;
+		int number;
+		string = reader.readLine();
+		if(!string.equals(null)){
+			while(true){
+				string = reader.readLine();
+				number = Integer.parseInt(string);
+				list.add(number);
+				if(string.equals(null))
+					System.out.println("Incorrect input");
+			}
+		}
+		else
+			System.out.println("Incorrect input");
+		
+		return list;
+	}
+}
