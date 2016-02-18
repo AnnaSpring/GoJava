efcba2904cdd048096c0ff8db93430398545b007
AnnaSpring <annavv.0595@gmail.com>
2016-02-18 02:11:04 +0200

    Test commit

---
+++ 1 Module Java Basics/src/anagram/Anagram.java
@@ -0,0 +1,8 @@
+package anagram;
+
+public class Anagram {
+	public static void main(String[] args){
+		System.out.println("Amazing code will be here");
+	}
+
+}
--- 1 Module Java Basics/src/distanceBetween/Distance.java
+++ 1 Module Java Basics/src/distanceBetween/Distance.java
@@ -9,6 +9,7 @@ public class Distance {
 		inputDigits(list);
 		for(int i = 0; i< list.size(); i++){
 			System.out.println(list.get(i));
+			System.out.println(list.get(i));
 		}
 		
 	}
