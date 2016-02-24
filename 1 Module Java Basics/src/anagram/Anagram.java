87862ff5c92d971d2db67bedf54a2947a978ae02
AnnaSpring <annavv.0595@gmail.com>
2016-02-24 16:07:30 +0200

    final version

--- 1 Module Java Basics/src/anagram/Anagram.java
+++ 1 Module Java Basics/src/anagram/Anagram.java
@@ -4,18 +4,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Anagram {
-	public static void main(String[] args)throws Exception{
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String userInput = reader.readLine();
-		String[] arrayOfWords = userInput.split(" ");
-		for(int i = 0; i < arrayOfWords.length; i++){
-			System.out.print(outputAnagram(arrayOfWords[i].toString()) + " ");
-		}
-	}
-	
+    public static void main(String[] args)throws Exception{
 
-	public static String outputAnagram(String string){
-		return new StringBuilder(string).reverse().toString();
-	}
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String userInput = reader.readLine();
+        String[] arrayOfWords = userInput.split(" ");
+        for(int i = 0; i < arrayOfWords.length; i++){
+            System.out.print(outputAnagram(arrayOfWords[i].toString()) + " ");
+        }
+    }
+
+    
+    public static String outputAnagram(String string){
+        return new StringBuilder(string).reverse().toString();
+    }
 }
--- 1 Module Java Basics/src/helloworld/Hello.java
+++ 1 Module Java Basics/src/helloworld/Hello.java
@@ -2,7 +2,7 @@ package helloworld;
 
 
 public class Hello {
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+    }
 }
