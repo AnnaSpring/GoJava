f5aaefe957281150bfff90b2223200226f2d3c97
AnnaSpring <annavv.0595@gmail.com>
2016-05-25 17:48:18 +0300

    get quotes using JPA

--- Kickstarter/Kickstarter/src/main/java/anna/kickstarter/dao/quotedao/QuoteDAOSql.java
+++ Kickstarter/Kickstarter/src/main/java/anna/kickstarter/dao/quotedao/QuoteDAOSql.java
@@ -1,33 +1,21 @@
 package anna.kickstarter.dao.quotedao;
 
-import java.util.Random;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
 
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Projections;
 import org.springframework.stereotype.Repository;
 
-import anna.kickstarter.dao.HibernateUtil;
 import anna.kickstarter.domain.Quote;
 
 @Repository
 public class QuoteDAOSql implements QuoteDAO {
 
-    public Quote getRandomQuote() {      
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        Session session = sessionFactory.openSession();
+    @PersistenceContext
+    private EntityManager em;
 
-        Quote result = null;  // will later contain a random entity
-        Criteria crit = session.createCriteria(Quote.class);
-        crit.setProjection(Projections.rowCount());
-        int count = ((Number) crit.uniqueResult()).intValue();
-        if (count != 0) {
-            int index = new Random().nextInt(count);
-            crit = session.createCriteria(Quote.class);
-            result = (Quote)(crit.setFirstResult(index).setMaxResults(1).uniqueResult());
-            session.close();
-        }
-        return result;
+    @Transactional(readOnly = true)
+    public Quote getRandomQuote() {
+        return em.find(Quote.class, 1);
     }
 }
--- Kickstarter/Kickstarter/src/main/java/anna/kickstarter/domain/Quote.java
+++ Kickstarter/Kickstarter/src/main/java/anna/kickstarter/domain/Quote.java
@@ -1,8 +1,22 @@
 package anna.kickstarter.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "quotes")
 public class Quote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private int id;
+    @Column
     private String quote;
+    @Column
     private String author;
     
     public void setQuote(String quote) {
